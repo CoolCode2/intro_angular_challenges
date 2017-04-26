d528832af3ede307b482cf19fcd715a0be0a5410
CoolCode2 <wcole2222@gmail.com>
2017-04-26 09:03:13 -0600

    im submitting this

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,20 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
+ app.controller("WelcomeController", WelcomeController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+function WelcomeController(){
+  this.full_name = "Cole";
+  this.age = 29;
+  this.city = "Santa Cruz";
+  this.state= "California";
+}
+
+function cont(){
+	this.class_name = "Cohort 3" ;
+	this.enrolled_students= "12";
+	this.start_date= "3/11";
+	this.end_date="6/2";
+	this.daysRemaining(42);
+}
+
