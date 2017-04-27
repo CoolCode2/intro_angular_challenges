5e77782e5cb581e808352bf278f135e6b1bf01c7
CoolCode2 <wcole2222@gmail.com>
2017-04-26 18:13:32 -0600

    thru 3 part 2

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -2,19 +2,32 @@ console.log('challenge #2 -- app.js loaded!');
 
 var app = angular.module("appTwo", []);
  app.controller("WelcomeController", WelcomeController);
+ app.controller("ClassController", ClassController); //allows angular to be used on other page
 
 function WelcomeController(){
   this.full_name = "Cole";
   this.age = 29;
   this.city = "Santa Cruz";
+  this.shout = function(name){
+  	return name.toUpperCase() +"!";
+  };
   this.state= "California";
 }
 
-function cont(){
-	this.class_name = "Cohort 3" ;
-	this.enrolled_students= "12";
-	this.start_date= "3/11";
-	this.end_date="6/2";
-	this.daysRemaining(42);
+function ClassController(){
+	this.class_name = "wdi" ;
+	this.enrolled_students= ["alexi","jess","cole","billy","cam","jacey","tyler","alex","troy"];
+	this.start_date= "3/13/2017";
+	this.end_date="6/2/2017";
+	this.daysRemaining = function(){
+		
+		var oneDay = 1000*24*60*60;
+		var months = Date.parse(this.end_date)-Date.now();
+		console.log(months);
+		var days = Math.round(months/oneDay);
+		console.log(days);
+		return days;
+	};
+	
 }
 
